1d7888f0ca9caf5e50a5b5ecc700f396760fcdaa
Ionut Sanda <ionut.sanda@siemens.com>
2022-01-13 17:06:18 +0200

    Add project files.

---
+++ Sanda_Ionut_Lab10/Data/ShoppingListDatabase.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Sanda_Ionut_Lab10.Models;
+
+namespace Sanda_Ionut_Lab10.Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShopList>().Wait();
+        }
+
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>()
+                .Where(i => i.ID == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveShopListAsync(ShopList sList)
+        {
+            if(sList.ID != 0)
+            {
+                return _database.UpdateAsync(sList);
+            } else
+            {
+                return _database.InsertAsync(sList);
+            }
+        }
+
+        public Task<int> DeleteShopListAsync(ShopList sList)
+        {
+            return _database.DeleteAsync(sList);
+        }
+    }
+}
---
+++ Sanda_Ionut_Lab10/ListPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanda_Ionut_Lab10.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sanda_Ionut_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var sList = (ShopList)BindingContext;
+            sList.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(sList);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var sList = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(sList);
+            await Navigation.PopAsync();
+        }
+
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
